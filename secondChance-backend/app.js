3f0ff3751192bb051941610b21bf7a60f3e6b29d
allenyonemoto <allenyonemoto@icloud.com>
2024-06-27 02:14:48 -0400

    Changes done

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -2,18 +2,18 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const pinoLogger = require('./logger')
-//const path = require('path')
+//  const path = require('path')
 const connectToDatabase = require('./models/db')
-//const { loadData } = require('./util/import-mongo/index')
+//  const { loadData } = require('./util/import-mongo/index')
 
 const app = express()
 app.use('*', cors())
 const port = 3060
 
 // Connect to MongoDB; we just do this one time
-connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB')
-})
+connectToDatabase().then(() => { pinoLogger.info('Connected to DB') })
+
+
 .catch((e) => console.error('Failed to connect to DB', e))
 app.use(express.json())
 
@@ -32,8 +32,7 @@ app.use('/api/auth', authRoutes)
 app.use('/api/secondchance/search', searchRoutes)
 
 // Global Error Handler
-app.use((err, req, res, next) => { console.error(err); 
-    res.status(500).send('Internal Server Error') })
+app.use((err, req, res, next) => { console.error(err), res.status(500).send('Internal Server Error') })
 
 app.get('/', (req, res) => { res.send('Inside the server') })
 
