1df24e8ecea75ffb62254369d0d943fb7519c351
allenyonemoto <allenyonemoto@icloud.com>
2024-06-27 02:10:49 -0400

    Changes done

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const pinoLogger = require('./logger')
-const path = require('path')
+//const path = require('path')
 const connectToDatabase = require('./models/db')
-const { loadData } = require('./util/import-mongo/index')
+//const { loadData } = require('./util/import-mongo/index')
 
 const app = express()
 app.use('*', cors())
@@ -12,9 +12,9 @@ const port = 3060
 
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
-    pinoLogger.info( 'Connected to DB' )
+    pinoLogger.info('Connected to DB')
 })
-.catch( (e) => console.error( 'Failed to connect to DB', e ) )
+.catch((e) => console.error('Failed to connect to DB', e))
 app.use(express.json())
 
 // Route files
@@ -32,7 +32,8 @@ app.use('/api/auth', authRoutes)
 app.use('/api/secondchance/search', searchRoutes)
 
 // Global Error Handler
-app.use((err, req, res, next) => { console.error(err), res.status(500).send('Internal Server Error') })
+app.use((err, req, res, next) => { console.error(err); 
+    res.status(500).send('Internal Server Error') })
 
 app.get('/', (req, res) => { res.send('Inside the server') })
 
