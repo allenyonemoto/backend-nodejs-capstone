9eba6d4d2d557ed0166c6c2f828b6a08cebaf79f
allenyonemoto <allenyonemoto@icloud.com>
2024-06-27 02:41:05 -0400

    Changes done

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -31,8 +31,8 @@ app.use('/api/secondchance/search', searchRoutes)
 
 // Global Error Handler
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
+  console.error(err)
+  res.status(500).send('Internal Server Error')
 })
 
 app.get('/', (req, res) => { res.send('Inside the server') })
