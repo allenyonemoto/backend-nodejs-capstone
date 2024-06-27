f8a49e913757b72f0c2cc3ddfa131445203edcb1
allenyonemoto <allenyonemoto@icloud.com>
2024-06-27 02:03:50 -0400

    Changes done

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -1,4 +1,3 @@
-/*jshint esversion: 8*/
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
@@ -16,7 +15,6 @@ connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB')
 })
 .catch((e) => console.error('Failed to connect to DB', e))
-
 app.use(express.json())
 
 // Route files
@@ -39,10 +37,6 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal Server Error')
 })
 
-app.get('/', (req, res) => {
-    res.send('Inside the server')
-})
+app.get('/', (req, res) => {res.send('Inside the server')})
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+app.listen(port, () => {console.log(`Server running on port ${port}`)})
